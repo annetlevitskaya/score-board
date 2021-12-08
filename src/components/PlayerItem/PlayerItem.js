82520446294a440dba08d3d9551712c2caa7bac6
annetlevitskaya <annetlevitskaya@gmail.com>
2021-12-08 13:40:56 +0200

    add automatic sort for players list

--- src/components/PlayerItem/PlayerItem.js
+++ src/components/PlayerItem/PlayerItem.js
@@ -1,22 +1,48 @@
 import React from 'react';
 import './PlayerItem.modules.css';
 
+const sortByASC = (first, second) => {
+    return second?.score - first?.score;
+};
+const sortByDESC = (first, second) => {
+    return first?.score - second?.score;
+};
+
+
 class PlayerItem extends React.Component {
+    state = {
+        players: [],
+        sortType: 'ASC',
+    };
+
+    componentDidMount() {
+        const source = new EventSource('http://localhost:5000/');
+        source.onmessage = e => {
+            this.addPlayer(JSON.parse(e.data));
+        };
+    }
+
+    addPlayer = player => this.setState(({ players }) => ({
+        players: [...players, player]
+    }))
 
     render() {
-        return (
+        const { players, sortType } = this.state; 
+        const sortedPlayerList = players.sort(sortType === 'ASC' ? sortByASC : sortByDESC);
+
+        return sortedPlayerList.map(player => (
             <section className="player__row">
                 <div className="player__row_left">
                     <div className="player__row_avatar_box">
-                        <img src="" className="player__row_avatar"/>
+                        <img src={player?.avatar} className="player__row_avatar"/>
                     </div>
-                    <p className="player__row_name">Player name</p>
+                    <p className="player__row_name">{player?.name}</p>
                 </div>
                 <div className="player__row_score">
-                    21
+                    {player?.score}
                 </div>
             </section>
-        )
+        ))
     }
 }
 
